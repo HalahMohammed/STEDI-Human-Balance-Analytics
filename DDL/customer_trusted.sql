58d59a66a75d57fb4773caeddb46b8422699a009
Halah Abousohh <eng.halah.mohammed212@outlook.sa>
2026-04-29 01:14:08 +0200

    Create external table for trusted customers data

---
+++ DDL/customer_trusted.sql
@@ -0,0 +1,15 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`customers_trusted` (
+  `registrationdate` bigint,
+  `customername` string,
+  `birthday` string,
+  `sharewithfriendsasofdate` bigint,
+  `lastupdatedate` bigint,
+  `email` string,
+  `serialnumber` string,
+  `phone` string,
+  `sharewithresearchasofdate` bigint,
+  `sharewithpublicasofdate` bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES ('serialization.format' = '1')
+LOCATION 's3://databucketdemo53/customer_trusted/'
