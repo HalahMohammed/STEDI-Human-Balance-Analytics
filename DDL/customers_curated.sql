c24929c5dba9412760e745b4a82c76640c1a3012
Halah Abousohh <eng.halah.mohammed212@outlook.sa>
2026-04-29 01:14:52 +0200

    Update S3 location for customers_curated.sql

--- DDL/customers_curated.sql
+++ DDL/customers_curated.sql
@@ -12,5 +12,5 @@ CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`customers_curated` (
 )
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 WITH SERDEPROPERTIES ('serialization.format' = '1')
-LOCATION 's3://databucketdemo53/customer_trusted/'
+LOCATION 's3://databucketdemo53/customer_curated/'
 TBLPROPERTIES ('has_encrypted_data' = 'false');
